dc65b7b2eace7eb90f2351911403e3af4adcc7c2
jack <1286783182@qq.com>
2023-04-07 14:07:27 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Layout from './+layout.svelte';
+	import Header from './header.svelte';
+</script>
+
+<Header />
+<hr />
+7
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
